2ed8ec60e5f06e38732404ec0b6b8ed358ad2828
Lucas Melo <lucasmelodos322@gmail.com>
2020-09-10 18:27:20 -0300

    criando header responsivo
    Co-authored-by: peHaick <pedrohaickzz@gmail.com>

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,29 @@
-
 import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap'
 import './style.css';
+import '../../index.css';
+import  minaLogo  from '../../assets/images/minaLogo.png';
 
-const Header = () => (
-  <header className="header">
-    <h1>Usuários</h1>
-  </header>
-);
+function Header() {
+  return (
+    <>
+      <Navbar bg="white" expand="md">
+        <Navbar.Brand href="/"><img className="logo" src={minaLogo} alt="logo" /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Cursos</Nav.Link>
+            <Nav.Link href="#link">Como funciona</Nav.Link>
+            <Nav.Link href="#link">A Iniciativa Minacademy</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <button className="login-button">
+          Entrar
+        </button>
+      </Navbar>
+    </>
+
+  );
+}
 
 export default Header;
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import HookComponent from './components/HookComponent';
-import ReduxComponent from './components/ReduxComponent';
 import './style.css';
+import Header from '../../components/Header'
 
-const Home = ({ hooks }) => hooks ? <HookComponent/> : <ReduxComponent/>;
-
-Home.propTypes = {
-  hooks: PropTypes.bool
-};
-
-Home.defaultProps = {
-  hooks: false
+function Home() {
+  return(
+    <Header/>
+  );
 }
 
 export default Home;
