cde28964a85c58f9237d73d6e3c08b71490f9a49
Eduardo Picolo <eduardonpicolo@gmail.com>
2020-09-23 11:28:50 -0300

    Activity: Implementado funcionalidades basicas do componente
    - Conectado a store Tutorial
    - Design basico
    
    https://github.com/fga-eps-mds/2020.1-Minacademy-Wiki/issues/52

--- src/views/Tutorial/components/Activity/index.js
+++ src/views/Tutorial/components/Activity/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import { Link, useRouteMatch } from 'react-router-dom';
+import React, {useEffect} from 'react';
+import { connect } from 'react-redux'
+import { selectActivity } from '../../../../slices/tutorialSlice'
+import { Link } from 'react-router-dom';
 import './style.scss';
+import Button from '../../../../components/Button';
 
-function Activity () {
-  const match = useRouteMatch();
+function Activity ({ activity }) {
+  useEffect(() => {
+    (async function teste() {
+      console.log(activity)
+    })()
+  });
+
   return (
     <div className="activity">
-      <h2>Componente_atividade </h2>
-      <p>
-        <Link to="/tutorial">Voltar</Link>
-      </p>
+      <div className="activity__description">
+        <h4> { activity.description } </h4>
+      </div>
+      <div className="activity__body">
+        {Object.keys(activity.alternatives)
+          .map(item => (
+            <p key={item}><b>{item}</b>. {activity.alternatives[item]}</p>
+        ))}
+      </div>
+      <div className="activity__action-bar">
+        <Button inverted shadow>
+          Responder
+        </Button>
+      </div>
     </div>
   );
 }
 
-export default Activity
+const mapStateToProps = (state, props) => ({
+    activity: selectActivity(state, props)
+})
+
+export default connect(mapStateToProps)(Activity)
