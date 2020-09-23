ef6d64545e9c97d7d7204c11bd2777683d77f774
Eduardo Picolo <eduardonpicolo@gmail.com>
2020-09-23 11:26:46 -0300

    Redux: Implementado selectors para atividades
    - Essa modificação contém codigo placeholder de atividade, a fim de
      exemplificar uma conexão com o banco de dados
    
    https://github.com/fga-eps-mds/2020.1-Minacademy-Wiki/issues/52

--- src/slices/tutorialSlice.js
+++ src/slices/tutorialSlice.js
@@ -1,7 +1,25 @@
 import { createSlice, createSelector } from '@reduxjs/toolkit';
 
 const initialState = {
-  currentModule: 1
+  currentModule: 1,
+  activities: [{
+    id: 999,
+    number: 1,
+    description: 'Lorem ipsum?',
+    alternatives: {
+      a: 'lorem',
+      b: 'ipsum',
+      c:'dolor'}
+    },
+  {
+    id: 111,
+    number: 2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus nec erat quis lorem suscipit gravida. Aenean commodo ex nulla, et tristique nisi cursus nec?',
+    alternatives: {
+      a: 'uma opção',
+      b: 'outra opção',
+      c: 'teste opção'}
+  }],
 };
 
 const tutorial = createSlice({
@@ -18,9 +36,23 @@ const tutorial = createSlice({
 });
 
 const selectTutorial = state => state.tutorial;
+const getActivity = (state, props) => {
+  return state.tutorial.activities.find(activity =>
+      activity.number == props.match.params.activityNumber
+    )
+}
+
 export const selectCurrentModule = createSelector(
   [selectTutorial],
   tutorial => tutorial.currentModule
+)
+export const selectActivity = createSelector(
+  [getActivity],
+  activity => activity
+)
+export const selectActivitiesList = createSelector(
+  [selectTutorial],
+  tutorial => tutorial.activities.map(activity => ({ number: activity.number}))
 )
 
 export default tutorial.reducer;
