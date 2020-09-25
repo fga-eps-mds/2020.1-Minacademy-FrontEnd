e229e17f3346f52feb6dcafaf158e361330f7e99
Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>
2020-09-25 06:29:48 -0300

    Escolhendo ambiente

---
+++ src/services/ambiente.js
@@ -0,0 +1,14 @@
+export default baseURL = () => {
+
+  switch (process.env.NODE_ENV) {
+    case 'development':
+      return 'http://localhost:9000';
+    case 'production':
+      return '';
+    case 'homolog':
+      return 'http://159.89.158.50:9000'
+    default:
+      return '';
+  };
+
+};
--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,4 @@
 import axios from 'axios';
+import baseURL from './ambiente';
 
 export default axios.create({ baseURL, withCredentials: true });
