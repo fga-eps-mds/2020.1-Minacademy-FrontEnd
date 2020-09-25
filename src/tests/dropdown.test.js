a69ce7ee3868ce353685a3f66ac1f20173bcb80b
Lucas Melo <lucasmelodos322@gmail.com>
2020-09-25 00:08:22 -0300

    Adicao de testes unitarios
    Co-authored-by: estevaoreis25 <estevaoreis1587@gmail.com>

--- src/tests/dropdown.test.js
+++ src/tests/dropdown.test.js
@@ -1,7 +1,8 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 import Dropdown from '../components/Dropdown';
+import { store } from '../store';
 
 it('Expect to render Dropdown component', () => {
-  expect(shallow(<Dropdown></Dropdown>).length).toEqual(1)
+  expect(shallow(<Dropdown store={store}></Dropdown>).length).toEqual(1)
 })
--- src/tests/profile.test.js
+++ src/tests/profile.test.js
@@ -1,16 +1,18 @@
-import { shallow } from 'enzyme';
+import { shallow, mountW } from 'enzyme';
 import React from 'react';
 import Profile from '../views/Profile';
 import 'mutationobserver-shim';
+import { store } from '../store'
 
 global.MutationObserver = window.MutationObserver;
 
 it('Expect to render Login Component', () => {
-  expect(shallow(<Profile></Profile>).length).toEqual(1)
-  expect(shallow(<Profile></Profile>)).toMatchSnapshot()
+  expect(shallow(<Profile store={store}></Profile>).length).toEqual(1)
+  expect(shallow(<Profile store={store}></Profile>)).toMatchSnapshot()
 })
 
 it('Expect to click button', () => {
-    const wrapper = shallow(<Profile />);
-    wrapper.find('Button').simulate('click')
+    const wrapper = shallow(<Profile store={store} />);
+    wrapper.dive('Button').simulate('click')
   })
+
