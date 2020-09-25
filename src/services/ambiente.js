bf7de6fef7f929ee2c03ba9d94c7de8a5ad6c578
Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>
2020-09-25 06:35:04 -0300

    Corrigindo função

--- src/services/ambiente.js
+++ src/services/ambiente.js
@@ -1,14 +1,20 @@
-export default baseURL = () => {
+const ambiente = () => {
 
   switch (process.env.NODE_ENV) {
+
     case 'development':
       return 'http://localhost:9000';
+
     case 'production':
       return '';
+
     case 'homolog':
       return 'http://159.89.158.50:9000'
+
     default:
       return '';
   };
 
 };
+
+export default baseURL();
