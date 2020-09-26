2b6b268884987eac2f021a25a323a61a28043156
Eduardo Picolo <eduardonpicolo@gmail.com>
2020-09-25 21:55:34 -0300

    Teste: Corrigido testes para componente Tutorial

--- src/tests/tutorial.test.js
+++ src/tests/tutorial.test.js
@@ -1,7 +1,22 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 import Tutorial from '../views/Tutorial';
+import { MemoryRouter } from 'react-router-dom'
 
 it('Expect to render Tutorial component', () => {
-  expect(shallow(<Tutorial></Tutorial>).length).toEqual(1)
+  expect(
+    shallow(
+      <MemoryRouter>
+        <Tutorial />
+      </MemoryRouter>
+    ).length).toEqual(1)
+
+    expect(
+      shallow(
+        <MemoryRouter>
+          <Tutorial />
+        </MemoryRouter>
+      )
+    ).toMatchSnapshot()
+
 })
