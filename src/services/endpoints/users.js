4f5482c69732109d42a00d2d45e29d8c1d16dfa1
estevaoreis <estevaoreis1587@gmail.com>
2020-09-30 08:12:45 -0300

    Criação de endpoints para a pagna de recuperação de senha e de mudança de senha

--- src/services/endpoints/users.js
+++ src/services/endpoints/users.js
@@ -1,11 +1,15 @@
 const USER_ENDPOINT = '/users';
 const LOGIN_ENDPOINT = '/users/login'
 const LOGOUT_ENDPOINT = '/users/logout'
-const PROFILE_ENDPOINT = '/editUser/'
+const PROFILE_ENDPOINT = `/editUser/`
+const RESET_PASS_ENDPOINT = '/resetPass'
+const CHANGE_PASS_ENDPOINT = '/changePass'
 
 export {
   USER_ENDPOINT,
   LOGIN_ENDPOINT,
   LOGOUT_ENDPOINT,
   PROFILE_ENDPOINT,
+  RESET_PASS_ENDPOINT,
+  CHANGE_PASS_ENDPOINT
 }
