5c61c0137695c999418989b504e3fb746c2af4bb
Eduardo Picolo <eduardonpicolo@gmail.com>
2020-10-01 15:02:43 -0300

    Header: Melhorias visuais na navbar
    - Removido bootstrap da navbar
    - Ajustes nas proporções
    - Responsividade
    - Corrigido antigo dropdown do bootstrap causando rolagem horizontal
    
    Co-authored-by: luucas-melo <lucasmelodos322@gmail.com>

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,74 +1,187 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { selectCurrentUser } from '../../slices/usersSlice'
-import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
-import './style.scss';
-import minaLogo from '../../assets/images/minacademyLogo.svg';
-import notification from '../../assets/images/notification.svg'
-import Button from '../Button';
 import { useHistory, Link } from 'react-router-dom';
-import '../../index.css';
+import { selectCurrentUser } from '../../slices/usersSlice';
 import { logout } from '../../services/usersService';
+import Button from '../Button';
+import { ReactComponent as Logo } from '../../assets/images/minacademyLogo.svg';
+import { ReactComponent as Bell } from '../../assets/images/notification.svg';
+import './style.scss';
 
 function Header({ currentUser, logout }) {
   const history = useHistory();
+  const [hidden, setHidden] = useState(true)
+
+  const responsive = () => {
+    var nav = document.getElementsByClassName('header__navigation');
+    console.log(nav);
+    if (nav.length > 0) {
+      nav[0].className += '-responsive';
+    } else {
+      nav = document.getElementsByClassName('header__navigation-responsive');
+      nav[0].className = 'header__navigation';
+    }
+  };
+
   return (
-    <>
-      <Navbar bg="white" expand="md">
-        <Navbar.Brand href="/"><img className="logo" src={minaLogo} alt="logo" /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            {currentUser ? (<>
-              <Link to="/dashboard">Dashboard</Link>
-              <Link to="/tutorial">Tutorial</Link>
-              <Link to="/">Fórum</Link>
-              <Link to="/">Ranking</Link>
-            </>
-            ) :
-              (<>
-                <Link to="/tutorial">Tutorial</Link>
-                <Link to="#infoBar2">Como funciona</Link>
-                <Link to="/">A Iniciativa Minacademy</Link>
-              </>
-              )}
-          </Nav>
-        </Navbar.Collapse>
+    <div className="header">
+      <Link className="header__logo" to="/">
+        <Logo className="name" />
+      </Link>
+
+      <div className="header__navigation">
         {currentUser ? (
           <>
-            <img className="notification" src={notification} alt="logo" />
-            <NavDropdown title={currentUser.name}>
-              <Link to="/perfil">Perfil</Link>
-              <Link to="/">Certificados</Link>
-              <Link onClick={() => logout()}>Sair</Link>
-            </NavDropdown>
+            <Link className="header__navigation-option" to="/dashboard">
+              Dashboard
+            </Link>
+            <Link className="header__navigation-option" to="/tutorial">
+              Tutorial
+            </Link>
+            <Link className="header__navigation-option" to="/forum">
+              Fórum
+            </Link>
           </>
-        )
-          : (
-            <div className="nav-buttons">
-              <Button onClick={() => { history.push('/cadastro') }} small>
-                Cadastrar
-              </Button>
-              <Button onClick={() => { history.push('/login') }} inverted color small>
-                Entrar
-              </Button>
+        ) : (
+          <>
+            <Link className="header__navigation-option" to="/">
+              Tutorial
+            </Link>
+            <Link className="header__navigation-option" to="/">
+              Como funciona
+            </Link>
+            <Link className="header__navigation-option" to="/">
+              A Iniciativa Minacademy
+            </Link>
+          </>
+        )}
+      </div>
+
+      <div className="header__navigation-action">
+        {currentUser ? (
+          <>
+            <div>
+              <Bell className="header__navigation-action-icon" />
+              <span className="header__navigation-action-name">{currentUser.name}</span>
+              <span className="header__navigation-action-dropdown"
+                onClick={() => setHidden(!hidden)}
+              >
+                icon
+              </span>
+              {hidden ? null : (
+                <div className="nav-dropdown">
+                  <div className="nav-dropdown__items">
+                    <Link
+                      onClick={() => setHidden(!hidden)}
+                      className="nav-dropdown__items-item"
+                      to="/perfil"
+                    >
+                      Perfil
+                    </Link>
+                    <Link
+                      onClick={() => setHidden(!hidden)}
+                      className="nav-dropdown__items-item"
+                      to="/certificados"
+                    >
+                      Certificados
+                    </Link>
+                    <Link
+                      onClick={() => {
+                        setHidden(!hidden)
+                        logout()
+                      }}
+                      className="nav-dropdown__items-item"
+                      to="/"
+                    >
+                      Sair
+                    </Link>
+                  </div>
+                </div>
+              )}
             </div>
+          </>
+        ) : (
+          <>
+            <Button
+              onClick={() => {
+                history.push('/login');
+              }}
+              inverted
+              color
+              small
+            >
+              Entrar
+            </Button>
+            <Button
+              onClick={() => {
+                history.push('/cadastro');
+              }}
+              inverted
+              color
+              small
+            >
+              Cadastrar
+            </Button>
+          </>
+        )}
+        <div className="icon" onClick={responsive}>
+          icon
+        </div>
+      </div>
+    </div>
+    // <>
+    //   <Navbar bg="white" expand="md">
+    //     <Navbar.Brand href="/"><img className="logo" src={minaLogo} alt="logo" /></Navbar.Brand>
+    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //     <Navbar.Collapse id="basic-navbar-nav">
+    //       <Nav className="mr-auto">
+    //         {currentUser ? (<>
+    //           <Link to="/dashboard">Dashboard</Link>
+    //           <Link to="/tutorial">Tutorial</Link>
+    //           <Link to="/">Fórum</Link>
+    //           <Link to="/">Ranking</Link>
+    //         </>
+    //         ) :
+    //           (<>
+    //             <Link to="/tutorial">Tutorial</Link>
+    //             <Link to="#infoBar2">Como funciona</Link>
+    //             <Link to="/">A Iniciativa Minacademy</Link>
+    //           </>
+    //           )}
+    //       </Nav>
+    //     </Navbar.Collapse>
+    //     {currentUser ? (
+    //       <>
+    //         <img className="notification" src={notification} alt="logo" />
+    //         <NavDropdown title={currentUser.name}>
+    //           <Link to="/perfil">Perfil</Link>
+    //           <Link to="/">Certificados</Link>
+    //           <Link onClick={() => logout()}>Sair</Link>
+    //         </NavDropdown>
+    //       </>
+    //     )
+    //       : (
+    //         <div className="nav-buttons">
+    //           <Button onClick={() => { history.push('/cadastro') }} small>
+    //             Cadastrar
+    //           </Button>
+    //           <Button onClick={() => { history.push('/login') }} inverted color small>
+    //             Entrar
+    //           </Button>
+    //         </div>
 
-          )}
-      </Navbar>
-    </>
-
+    //       )}
+    //   </Navbar>
+    // </>
   );
-
 }
 
 const mapStateToProps = state => ({
-  currentUser: selectCurrentUser(state)
-})
+  currentUser: selectCurrentUser(state),
+});
 
 const mapDispatchToProps = dispatch => ({
-  logout: () => dispatch(logout())
-})
-
+  logout: () => dispatch(logout()),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from "react-redux"
 import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from './store'
 import Routes from './routes/routes';
+import './app.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
