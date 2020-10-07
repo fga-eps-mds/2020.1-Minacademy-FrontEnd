1befaf16e63f43085fb11be0b2811fd685bf1629
Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>
2020-10-07 17:44:48 -0300

    Adicionando eslint como requisito de PR

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,22 @@
 module.exports = {
   env: {
-    es6: true,
+    browser: true,
+    es2021: true,
   },
   extends: [
+    'plugin:react/recommended',
     'airbnb',
-    'prettier',
-    'prettier/react'
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-    __DEV__: 'readonly'
-  },
-  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2018,
+    ecmaVersion: 12,
     sourceType: 'module',
   },
   plugins: [
     'react',
-    'prettier'
   ],
   rules: {
-    'prettier/prettier': 'error',
-    'react/jsx-filename-extension': [
-      'warn',
-      {
-        extensions: ['.jsx', '.js']
-      }
-    ],
-    'import/prefer-default-export': 'off',
-    'react/state-in-constructor': 'off',
-    'react/static-property-placement': 'off',
-    'react/jsx-props-no-spreading': 'off',
-    'react/prop-types': 'off',
-    'no-param-reassign': 'off',
-    'no-console': 'off'
   },
-}; 
+};
