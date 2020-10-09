4eb2ee6950f16a5d006a4471b9441ee0e9414795
Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>
2020-10-08 21:45:31 -0300

    Revertendo testes

--- src/tests/profile.test.js
+++ src/tests/profile.test.js
@@ -10,3 +10,13 @@ global.MutationObserver = window.MutationObserver;
 it('Expect to render Profile Component', () => {
   expect(shallow(<Profile  store={store} />).length).toEqual(1)
 })
+
+it('Expect to click button', () => {
+  const wrapper = shallow(<Profile store={store} />);
+  wrapper.dive('Button').simulate('click')
+})
+
+it('Expect to click button', () => {
+  const wrapper = shallow(<Profile store={store} />);
+  wrapper.dive('form').simulate('submit', {preventDefault: jest.fn()})
+})
