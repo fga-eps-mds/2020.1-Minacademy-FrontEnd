d90d8745591e8d5efc0528b39881c2e5665f6c0c
Gabriela Medeiros <gabriela.medeiros2010@hotmail.com.br>
2020-10-08 21:45:31 -0300

    Revertendo testes

--- src/tests/button.test.js
+++ src/tests/button.test.js
@@ -5,3 +5,8 @@ import Button from '../components/Button';
 it('Expect to render Button component', () => {
   expect(shallow(<Button />).length).toEqual(1)
 })
+
+it('Expect to click button', () => {
+  const wrapper = shallow(<Button />);
+  wrapper.find('button').simulate('click')
+})
