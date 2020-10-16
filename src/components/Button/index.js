11b968e34c9d26ad24fed01551b0d0795608ae57
luucas-melo <lucasmelodos322@gmail.com>
2020-10-16 19:50:37 -0300

    Button: adicionado novas props de error e success
    https://github.com/fga-eps-mds/2020.1-Minacademy-Wiki/issues/75
    
    Co-authored-by: EduardoPicolo <eduardonpicolo@gmail.com>

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './style.scss';
 
 
-function Button({ children, inverted, shadow , small, color, ...otherProps }) {
+function Button({ children, inverted, shadow , small, color, success, error,...otherProps }) {
     return (
         <button className={`
         ${inverted ? 'inverted' : ''}
         ${shadow ? 'thin-shadow' : ''}
         ${small ? 'small' : ''}
         ${color ? 'color' : ''}
+        ${success ? 'success' : ''}
+        ${error ? 'error' : ''}
          button`}
             {...otherProps}
         >
--- src/routes/components/publicRoute.js
+++ src/routes/components/publicRoute.js
@@ -6,7 +6,6 @@ import { useLocation } from 'react-router-dom'
 
 export const PublicRoute = ({ currentUser, component: Component, ...rest }) => {
     const location = useLocation();
-    console.log(location.pathname);
     return (
         <Route {...rest} component={(props) => (
             currentUser ? (
