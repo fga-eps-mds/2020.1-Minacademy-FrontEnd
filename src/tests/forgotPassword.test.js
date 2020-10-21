2c82f3de2d4655f47358c7aabb0c5bed9e04f66f
peHaick <pedrohaickzz@gmail.com>
2020-10-21 10:34:45 -0300

    Removendo testes falhos

--- src/tests/forgotPassword.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ForgotPassword from '../views/ForgotPassword'
-import {shallow} from 'enzyme'
-import { store } from '../store'
-
-it('Expect to render ForgotPassword Component', () => {
-    expect(shallow(<ForgotPassword store={store} />).length).toEqual(1)
-  })
