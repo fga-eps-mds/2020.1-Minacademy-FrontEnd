4ac02ebd4ac68df90cf7a654adf44ebe5f67a6cb
luucas-melo <lucasmelodos322@gmail.com>
2020-10-22 01:02:54 -0300

    Routes: Adicionado Header e Footer nos componentes de rotas publicas e privadas
    Co-authored-by: estevaoreis25 <estevaoreis1587@gmail.com>
    Co-authored-by: peHaick <pedrohaickzz@gmail.com>
    Co-authored-by: EduardoPicolo <eduardonpicolo@gmail.com>

--- src/routes/components/privateRoute.js
+++ src/routes/components/privateRoute.js
@@ -3,34 +3,37 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 import { selectCurrentUser } from '../../slices/usersSlice';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
-export const PrivateRoute = ({ currentUser, component: Component, ...rest }) => (
-  <Route {...rest} component={(props) => (
-    currentUser ? (
-      <Component {...props} />
-    ) : (
-        <Redirect to="/login" />
-      )
-  )}
-  />
+export const PrivateRoute = ({
+  currentUser,
+  component: Component,
+  ...rest
+}) => (
+  <>
+    <Header />
+    <Route
+      {...rest}
+      component={(props) =>
+        currentUser ? <Component {...props} /> : <Redirect to="/login" />
+      }
+    />
+    <Footer />
+  </>
 );
 
-const mapStateToProps = state => ({
-  currentUser: selectCurrentUser(state)
+const mapStateToProps = (state) => ({
+  currentUser: selectCurrentUser(state),
 });
-
 
 PrivateRoute.defaultProps = {
   currentUser: null,
 };
 
 PrivateRoute.propTypes = {
-  currentUser: PropTypes.oneOfType([
-    PropTypes.oneOf([null]),
-    PropTypes.object
-  ]),
-  component: PropTypes.elementType.isRequired
+  currentUser: PropTypes.oneOfType([PropTypes.oneOf([null]), PropTypes.object]),
+  component: PropTypes.elementType.isRequired,
 };
-
 
 export default connect(mapStateToProps)(PrivateRoute);
