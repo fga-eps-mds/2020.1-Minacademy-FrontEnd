a68f3eff32282bab9f3721335361839e190cb574
Eduardo Picolo <eduardonpicolo@gmail.com>
2020-10-21 22:32:12 -0300

    Toast: Adicionado id para evitar multiplos toasts com a mesma mensagem

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -12,31 +12,28 @@ import Profile from '../views/Profile';
 import PrivateRoute from './components/privateRoute';
 import PublicRoute from './components/publicRoute';
 import Dashboard from '../views/Dashboard';
-import Welcome from '../views/Welcome'
-import Mentorship from '../views/Mentorship'
-
+import Welcome from '../views/Welcome';
+import Mentorship from '../views/Mentorship';
 
 const Routes = () => {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <PublicRoute exact path="/login" component={Login} />
-          <PublicRoute exact path="/cadastro" component={Register} />
-          <PrivateRoute exact path="/bem-vindo" component={Welcome}/>
-          <PrivateRoute path="/tutorial" component={Tutorial} />
-          <PrivateRoute exact path="/mentoria" component={Mentorship} />
-          <PublicRoute exact path="/forgotPassword" component={ForgotPassword} />
-          <PublicRoute exact path="/change/:resetLink" component={Change} />
-          <PrivateRoute exact path="/perfil" component={Profile} />
-          <PrivateRoute exact path="/dashboard" component={Dashboard} />
-        </Switch>
-        <Footer />
-      </BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <PublicRoute exact path="/login" component={Login} />
+        <PublicRoute exact path="/cadastro" component={Register} />
+        <PrivateRoute exact path="/bem-vindo" component={Welcome} />
+        <PrivateRoute path="/tutorial" component={Tutorial} />
+        <PrivateRoute exact path="/mentoria" component={Mentorship} />
+        <PublicRoute exact path="/forgotPassword" component={ForgotPassword} />
+        <PublicRoute exact path="/change/:resetLink" component={Change} />
+        <PrivateRoute exact path="/perfil" component={Profile} />
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+      </Switch>
+      <Footer />
     </>
   );
-}
+};
 
 export default Routes;
