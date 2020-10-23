0e0fa2dbbb21dd32045ae7467989cb921a6e9899
luucas-melo <lucasmelodos322@gmail.com>
2020-10-23 01:35:16 -0300

    Certificado: Melhorada informacoes do certificado
    Co-authored-by: estevaoreis25 <estevaoreis1587@gmail.com>
    Co-authored-by: peHaick <pedrohaickzz@gmail.com>

--- src/views/Certificate/Components/LearnerCertificate/index.js
+++ src/views/Certificate/Components/LearnerCertificate/index.js
@@ -1,41 +1,72 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import './style.scss'
-// import { ReactComponent as Logo } from '../../../../assets/images/minacademyLogo.svg'
+import './style.scss';
+import { ReactComponent as Logo } from '../../../../assets/images/minacademyLogo.svg'
 
-function LearnerCertificate({name, lastname, completedModules, courseType, workload, conclusion }) {
+function LearnerCertificate({
+  name,
+  lastname,
+  completedModules,
+  courseType,
+  workload,
+  conclusion,
+}) {
 
-  const month = conclusion.getMonth()
-  const year = conclusion.getFullYear()
-  const day = conclusion.getDay()
-  useEffect(() => {
-
-  }, [])
+   
   return (
-    <div id='certificate' className='certificate'>
-      <div id='certificate-logo' className='certificate-logo'>
-        {/* <Logo/> */}
-        <h1>Minacademy</h1>
+    <div id="certificate" className="certificate">
+      <div id="certificate-logo" className="certificate-logo">
+        <div id='logo-container' className='logo-container'>
+          <Logo/>
+        </div>
+        {/* <h1>Minacademy</h1> */}
+
+
       </div>
-      <div id='certificate-content' className='certificate-content'>
-        <p>Certificamos que a aprendiz <b>{name} {lastname}</b> concluiu o tutorial e as atividades. </p>
-        <h3>tipo de certificado: <span>{courseType}</span></h3>
-        <h3>módulos completos: <span>{completedModules}</span></h3>
-        <h3>carga horária: <span>{workload}</span></h3>
-        <h3>Data de conclusão: <span>{day}/{month}/{year}</span></h3>
+      <div id="certificate-content" className="certificate-content">
+        <p>
+          Certificamos que a aprendiz{' '}
+          <b>
+            {name} {lastname}
+          </b>{' '}
+          concluiu o tutorial e as atividades.{' '}
+        </p>
+        <br/>
+        <h3>
+          tipo de certificado: <span>{courseType}</span>
+        </h3>
+        <br/>
+        <h3>
+          módulos completos: <span>{completedModules}</span>
+        </h3>
+        <br/>
+        <h3>
+          carga horária: <span>{workload}h</span>
+        </h3>
+        <br/>
+        <h3>
+          Data de conclusão: <span>{conclusion}</span>
+        </h3>
       </div>
     </div>
   );
 }
 
-
+LearnerCertificate.defaultProps = {
+  name: null,
+  lastname: null,
+  courseType: null,
+  completedModules: null,
+  workload: null,
+  conclusion: null,
+};
 
 LearnerCertificate.propTypes = {
-  name: PropTypes.string.isRequired,
-  lastname: PropTypes.string.isRequired,
-  courseType: PropTypes.string.isRequired,
-  completedModules: PropTypes.arrayOf(PropTypes.string).isRequired,
-  workload: PropTypes.number.isRequired,
-  conclusion: PropTypes.string.isRequired,
+  name: PropTypes.string,
+  lastname: PropTypes.string,
+  courseType: PropTypes.string,
+  completedModules: PropTypes.number,
+  workload: PropTypes.number,
+  conclusion: PropTypes.string,
 };
 export default LearnerCertificate;
--- src/views/Certificate/index.js
+++ src/views/Certificate/index.js
@@ -2,26 +2,40 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import LearnerCertificate from './Components/LearnerCertificate';
 import { getLearnerCertificate } from '../../services/certificatesServices';
-
+import Loader from '../../components/Loader'
 function Certificate() {
-  const [certificate, setCertificate] = useState([]);
+  
+  const [certificate, setCertificate] = useState(null);
   const { _id } = useParams();
 
   useEffect(() => {
     getLearnerCertificate(_id).then((data) => setCertificate(data.certificate));
   }, []);
 
-  const { user, workload, courseType, createdAt } = certificate;
+  
+  console.log(typeof certificate);
 
-  return (
-    <LearnerCertificate
-      name={user?.name}
-      courseType={courseType}
-      lastname={user?.lastname}
-      completedModules={user?.completedModules?.length}
-      workload={workload}
-      conclusion={createdAt}
-    />
-  );
+  if(certificate === null) {
+    return <Loader big/>
+  }
+  else{
+    const { user, workload, courseType, createdAt } = certificate
+    return (
+      <>
+        <LearnerCertificate
+          name={user.name}
+          courseType={courseType}
+          lastname={user.lastname}
+          completedModules={user.completedModules.length}
+          workload={workload}
+          conclusion={new Intl.DateTimeFormat('pt-BR', {
+            year: 'numeric',
+            month: 'short',
+            day: '2-digit',
+          }).format(new Date(Date.parse(createdAt)))}
+        />
+      </>
+    );
+  }
 }
 export default Certificate;
