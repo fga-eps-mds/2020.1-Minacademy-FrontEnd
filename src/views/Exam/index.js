334a17d078f7edc5213e06c71e275d676aa99d19
Eduardo Picolo <eduardonpicolo@gmail.com>
2020-10-28 04:41:03 -0300

    Exam: Conectado componente a store
    - Adicionado botão para finalizar prova
    - Adicionado modal de confirmação
    
    https://github.com/fga-eps-mds/2020.1-Minacademy-Wiki/issues/80
    
    Co-authored-by: estevaoreis25 <estevaoreis1587@gmail.com>
    Co-authored-by: peHaick <pedrohaickzz@gmail.com>

--- src/views/Exam/index.js
+++ src/views/Exam/index.js
@@ -1,41 +1,76 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Switch, Route, useRouteMatch } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import ActivitiesList from '../Tutorial/components/ActivitiesList';
-import { selectCompletedActivities, selectTotalProgress } from '../../slices/tutorialSlice';
-import ExamQuestion from './components/ExamQuestion'
-import ExamRules from './components/ExamRules'
-import './style.scss'
+import { selectTotalAnswers } from '../../slices/tutorialSlice';
+import { getProgress } from '../../services/tutorialServices';
+import { toggleModalVisible } from '../../slices/modalSlice'
+import ExamQuestion from './components/ExamQuestion';
+import Button from '../../components/Button';
+import Modal from '../../components/Modal';
+import ExamRules from './components/ExamRules';
+import './style.scss';
 
-function Exam({ completedActivities, totalProgress }) {
-  const match = useRouteMatch();
+function Exam({ getProgress, totalAnswers, toggleModalVisible, match }) {
+  useEffect(() => {
+    getProgress();
+  }, []);
 
   return (
-  <div className="exam">
+    <div className="exam">
       <div className="exam__header">
         <div>
           <h1>Avaliação</h1>
-            <p>Conclua com mais de 70% de acertos para ser validado como mentor</p>
+          <p>
+            Conclua com mais de 70% de acertos para ser validado como mentor
+          </p>
         </div>
         <div className="exam__header--progress">
-          0/10 questões respondidas
+          {totalAnswers}/10 questões respondidas
+
+            <Button
+              disabled={totalAnswers <  10}
+              onClick={() => {
+                toggleModalVisible();
+              }}
+              shadow
+            >
+              Finalizar avaliação
+            </Button>
+
         </div>
       </div>
       <div className="exam__body">
-        <ActivitiesList fetchQuestions={'exam'} />
+        <ActivitiesList exam={true} />
         <Switch>
-          <Route path={`${match.path}/atividades/:activityNumber`} component={() => <ExamQuestion />} />
+          <Route
+            path={`${match.path}/atividades/:activityNumber`}
+            render={() => <ExamQuestion />}
+          />
           <Route path={match.path} component={ExamRules} />
         </Switch>
+        <Modal
+            title={`Finalizar e enviar?`}
+            confirmMessage='sim'
+            closeMessage='cancelar'
+            onClose={() => toggleModalVisible()}
+            onConfirm={() => {}}
+        >
+
+        </Modal>
       </div>
-  </div>
+    </div>
   );
 }
 
-const mapStateToProps = state => ({
-  completedActivities: selectCompletedActivities(state),
-  totalProgress: selectTotalProgress(state)
-})
+const mapStateToProps = (state) => ({
+  totalAnswers: selectTotalAnswers(state),
+});
 
-export default connect(mapStateToProps)(Exam)
+const mapDispatchToProps = (dispatch) => ({
+  toggleModalVisible: () => dispatch(toggleModalVisible()),
+  getProgress: () => dispatch(getProgress())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Exam);
