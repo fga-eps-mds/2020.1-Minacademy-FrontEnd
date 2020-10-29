da3b8a76ff605286be0690c7800404bc87a10adc
luucas-melo <lucasmelodos322@gmail.com>
2020-10-29 03:22:37 -0300

    Certificado: conectado componente com a selector de certificado

--- src/views/CourseCertificates/index.js
+++ src/views/CourseCertificates/index.js
@@ -1,31 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Loader from '../../components/Loader';
 import CertificateList from './components/CertificateList';
 import { generateCertificate } from '../../services/certificatesServices';
+import { selectCertificate, loading } from '../../slices/certificateSlice';
 import { selectTotalProgress } from '../../slices/tutorialSlice';
 import './style.scss';
 
 /* eslint-disable no-shadow */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 
-function CourseCertificates({ totalProgress }) {
-  const [learnerCertificate, setLearnerCertificate] = useState(null);
-
+function CourseCertificates({
+  totalProgress,
+  certificate,
+  generateCertificate,
+  loading,
+}) {
   useEffect(() => {
     if (totalProgress === 100) {
-      generateCertificate().then((data) =>
-        setLearnerCertificate(data.learnerCertificate)
-      );
+      generateCertificate();
     }
   }, []);
-  if (learnerCertificate === null) {
+  if (certificate.certificate === null) {
     return (
       <div className="courseCertificates">
         <div className="courseCertificates__header">
           <h1>Certificados</h1>
         </div>
-        <h3>Você não possui nenhum certificado.</h3>
+        <h3>Você não possui certificados.</h3>
       </div>
     );
   }
@@ -36,18 +39,22 @@ function CourseCertificates({ totalProgress }) {
           <h1>Certificados</h1>
           <p>Nessa página é possível visualizar seus certificados</p>
         </div>
-
+        {loading && <Loader big />}
         <div className="courseCertificates__body">
-          <CertificateList
-            certificateType="Certificado de conclusão de Tutorial"
-            conclusionData={new Intl.DateTimeFormat('pt-BR', {
-              year: 'numeric',
-              month: 'long',
-              day: '2-digit',
-            }).format(new Date(Date.parse(learnerCertificate?.createdAt)))}
-            workload={learnerCertificate?.workload}
-            id={learnerCertificate._id}
-          />
+          {certificate.certificate ? (
+            <CertificateList
+              certificateType="Certificado de conclusão de Tutorial"
+              conclusionData={new Intl.DateTimeFormat('pt-BR', {
+                year: 'numeric',
+                month: 'long',
+                day: '2-digit',
+              }).format(
+                new Date(Date.parse(certificate?.certificate?.createdAt))
+              )}
+              workload={certificate?.certificate?.workload}
+              id={certificate?.certificate?._id}
+            />
+          ): null}
         </div>
       </div>
     </>
@@ -56,10 +63,16 @@ function CourseCertificates({ totalProgress }) {
 
 const mapStateToProps = (state) => ({
   totalProgress: selectTotalProgress(state),
+  certificate: selectCertificate(state),
+  loading: loading(state),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  generateCertificate: () => dispatch(generateCertificate()),
 });
 
 CourseCertificates.propTypes = {
-  totalProgress: PropTypes.number.isRequired
+  totalProgress: PropTypes.number.isRequired,
 };
 
-export default connect(mapStateToProps)(CourseCertificates);
+export default connect(mapStateToProps, mapDispatchToProps)(CourseCertificates);
