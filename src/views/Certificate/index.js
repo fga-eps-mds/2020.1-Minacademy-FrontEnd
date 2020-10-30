4574cb18982ead51a3ad753d010e1c882c47ecce
luucas-melo <lucasmelodos322@gmail.com>
2020-10-29 22:19:49 -0300

    Certificate: adicionado component de certificado de mentor
    Co-authored-by: Rocsantos <ro12062000@gmail.com>

--- src/views/Certificate/index.js
+++ src/views/Certificate/index.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import LearnerCertificate from './Components/LearnerCertificate';
+import MentorCertificate from './Components/MentorCertificate';
 import { getLearnerCertificate } from '../../services/certificatesServices';
+import './style.scss';
 
 function Certificate() {
-  
   const [certificate, setCertificate] = useState(null);
   const { _id } = useParams();
 
@@ -12,29 +13,43 @@ function Certificate() {
     getLearnerCertificate(_id).then((data) => setCertificate(data.certificate));
   }, []);
   /* eslint-disable no-else-return */
-  
+
   // console.log(typeof certificate);
 
-  if(certificate === null) {
-    return <div/>
-  }
-  else{
-    const { user, workload, courseType, createdAt } = certificate
+  if (certificate === null) {
+    return <div />;
+  } else {
+    const { user, workload, courseType, createdAt } = certificate;
     return (
       <>
-        <LearnerCertificate
-          name={user.name}
-          courseType={courseType}
-          lastname={user.lastname}
-          completedModules={user.completedModules.length}
-          workload={workload}
-          conclusion={new Intl.DateTimeFormat('pt-BR', {
-            year: 'numeric',
-            month: 'long',
-            day: '2-digit',
-          }).format(new Date(Date.parse(createdAt)))}
-          id={_id}
-        />
+        {courseType === 'Learner' ? (
+          <LearnerCertificate
+            name={user.name}
+            courseType={courseType}
+            lastname={user.lastname}
+            completedModules={user.completedModules.length}
+            workload={workload}
+            conclusion={new Intl.DateTimeFormat('pt-BR', {
+              year: 'numeric',
+              month: 'long',
+              day: '2-digit',
+            }).format(new Date(Date.parse(createdAt)))}
+            id={_id}
+          />
+        ) : (
+          <MentorCertificate
+            name={user.name}
+            courseType={courseType}
+            lastname={user.lastname}
+            workload={workload}
+            conclusion={new Intl.DateTimeFormat('pt-BR', {
+              year: 'numeric',
+              month: 'long',
+              day: '2-digit',
+            }).format(new Date(Date.parse(createdAt)))}
+            id={_id}
+          />
+        )}
       </>
     );
   }
