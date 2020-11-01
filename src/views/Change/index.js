8038592bd565467ca5a79dfc35a7c08de68c4d3e
peHaick <pedrohaickzz@gmail.com>
2020-11-01 18:47:04 -0300

    Adicionando verificação de senhas iguais

--- src/views/Change/index.js
+++ src/views/Change/index.js
@@ -3,58 +3,65 @@ import { Link, useParams } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { changeUserPassword } from '../../services/usersService';
 import Button from '../../components/Button';
+import Input from '../../components/FormField/components/Input'
 import '../../index.css';
 import './style.scss';
 
 
 function Change() {
-    const { handleSubmit, register, errors } = useForm();
-    const {resetLink} = useParams();
-    const onSubmit = (values)=>{
-        changeUserPassword({password: values.password, resetLink});
-    }
+  const { handleSubmit, register, errors, watch } = useForm();
+  const { resetLink } = useParams();
+  const onSubmit = (values) => {
+    changeUserPassword({ password: values.password, resetLink });
+  }
 
-    return (
-        <>
-        <div className="change">
-            <div className="change__form">
-                <h1>Troque sua senha</h1>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <p>Nova senha</p>
-                    <input 
-                        type="password" 
-                        name="password" 
-                        placeholder="●●●●●●●●"
-                        ref={register({
-                        required: "campo obrigatório",
-                        minLength: {
-                            value: 6,
-                            message: "tamanho mínimo é 6"
-                        }
-                    })}
-                    />
-                    {errors.password && <span className="danger">{errors.password.message}</span>}
-                    <p>Confirme a nova senha</p>
-                    <input 
-                        type="password" 
-                        name="password" 
-                        placeholder="●●●●●●●●"
-                        ref={register({
-                        required: "campo obrigatório",
-                        minLength: {
-                            value: 6,
-                            message: "tamanho mínimo é 6"
-                        }
-                    })}
-                    />
-                    {errors.password && <span className="danger">{errors.password.message}</span>}
-                    <Button small>SALVAR</Button>
-                    <p>Já tem login e senha? <Link to="/login">Entrar</Link></p>
-                </form>
-            </div>
+  return (
+    <>
+      <div className="change">
+        <div className="change__form">
+          <h1>Troque sua senha</h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <p className='new-password'>nova senha</p>
+            <Input
+              type="password"
+              name="password"
+              placeholder="nova senha"
+              errors={errors}
+              register={register({
+                required: 'Campo obrigatório',
+                validate: (value) =>
+                  value === watch('confirmPassword') || 'Senhas não coincidem',
+                minLength: {
+                  value: 6,
+                  message: "tamanho mínimo é 6"
+                },
+              })}
+              autoComplete="off"
+            />
+            <p className='confirm-password'>confirme a nova senha</p>
+            <Input
+              type="password"
+              name="confirmPassword"
+              placeholder="confirmar senha"
+              errors={errors}
+              register={register({
+                required: 'Campo obrigatório',
+                validate: (value) =>
+                  value === watch('password') || 'Senhas não coincidem',
+                minLength: {
+                  value: 6,
+                  message: "tamanho mínimo é 6"
+                },
+              })}
+              autoComplete="off"
+            />
+            <Button small>SALVAR</Button>
+            <p className='login-link'>Já tem login e senha? <Link to="/login">Entrar</Link></p>
+          </form>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
 
 export default Change;
