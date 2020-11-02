4a6aa424aba414a7b97c982fe1b38d6665309a04
peHaick <pedrohaickzz@gmail.com>
2020-11-02 18:54:29 -0300

    Alterando testes

--- src/tests/exam.test.js
+++ src/tests/exam.test.js
@@ -16,9 +16,16 @@ it('Expect to render Tutorial component', () => {
     ).length).toEqual(1)
 })
 
-// it('Expect to render Activity Component', () => {
-//   expect(shallow(<ExamQuestion store={store} />).length).toEqual(1)
-// })
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useParams: () => ({
+    id: 590
+  })
+}));
+
+it('Expect to render Activity Component', () => {
+  expect(shallow(<ExamQuestion store={store} />).length).toEqual(1)
+})
 
 it('Expect to render ActivitiesList Component', () => {
   expect(shallow(<ActivitiesList  store={store} />).length).toEqual(1)
