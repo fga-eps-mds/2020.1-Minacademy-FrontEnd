ceaa70c23c41c6f7198debe206ef0bc17381331d
Eduardo Picolo <eduardonpicolo@gmail.com>
2020-11-09 20:29:16 -0300

    CertificateTemplate: Criado componente base para certificado
    - Componente faz uso da lib react-pdf, que permite facilmente
      transforma-lo em pdf
    
    https://github.com/fga-eps-mds/2020.1-Minacademy-Wiki/issues/93

---
+++ src/views/Certificate/Components/CertificateTemplate/index.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Page, Text, Image, View, Document } from '@react-pdf/renderer';
+import LOGO from '../../../../assets/images/minademyLogo.png'
+import styles from './styles'
+
+const MyDocument = ({ certificateData, image }) => {
+  return (
+    <Document>
+      <Page size="A4" orientation="landscape" style={styles.page}>
+        <View style={styles.section}>
+          <View style={styles.certificate}>
+            <View style={styles.certificate.header} >
+              {image ? <img src={LOGO} style={styles.certificate.header.logo}/> : <Image style={styles.certificate.header.logo} src={LOGO} />}
+              <Text style={styles.certificate.header.title.emphasis}>
+              {certificateData?.courseType === 'Learner'
+                ? 'Certificado de Conclusão de Tutorial Minacademy'
+                : 'Certificado de Mentoria'
+              }
+              </Text>
+            </View>
+
+            <View style={styles.certificate.body}>
+              <Text style={styles.certificate.content}>
+                Certificamos que
+              </Text>
+              <Text style={styles.certificate.content.emphasis}>
+                {certificateData?.user.name} {certificateData?.user.lastname}
+              </Text>
+              <Text style={styles.certificate.content}>
+              {certificateData?.courseType === 'Learner'
+                  ? 'finalizou com sucesso o tutorial da Minacademy.'
+                  : 'prestou mentoria voluntária em nossa plataforma.'
+                }
+              </Text>
+            </View>
+
+            <View style={styles.certificate.footer}>
+              <View style={styles.certificate.footer.column}>
+                <Text style={styles.certificate.footer.column.content}>
+                  Emitido em
+                </Text>
+                <Text style={styles.certificate.footer.column.content.emphasis}>
+                  {certificateData?.createdAt}
+                </Text>
+              </View>
+              <View style={styles.certificate.footer.column}>
+                <Text style={styles.certificate.footer.column.content}>
+                  Curso
+                </Text>
+                <Text style={styles.certificate.footer.column.content.emphasis}>
+                  Django
+                </Text>
+              </View>
+              <View style={styles.certificate.footer.column}>
+                <Text style={styles.certificate.footer.column.content}>
+                  {certificateData?.courseType === 'Learner'
+                    ? 'Mentor'
+                    : 'Aprendiz'
+                  }
+                </Text>
+                <Text style={styles.certificate.footer.column.content.emphasis}>
+                  {certificateData?.assignedPartner.name} {certificateData?.assignedPartner.lastname}
+                </Text>
+              </View>
+            </View>
+
+          </View>
+        </View>
+      </Page>
+    </Document>
+  );
+};
+
+export default MyDocument;
---
+++ src/views/Certificate/Components/CertificateTemplate/styles.js
@@ -0,0 +1,121 @@
+import { StyleSheet } from '@react-pdf/renderer';
+
+
+const styles = StyleSheet.create({
+  page: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30,
+    backgroundColor: '#fcfcfc',
+  },
+  section: {
+    width: 100+'%',
+    height: 100+'%',
+  },
+  certificate: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    color: '#ffffff',
+
+    header: {
+      backgroundColor: '#efefef',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      alignContent: 'center',
+      width: 100+'%',
+      paddingTop: 20,
+      paddingBottom: 20,
+      paddingLeft: 0,
+      paddingRight: 20,
+      marginBottom: 10+'vh',
+
+      date: {
+        display: 'flex',
+        flexDirection: 'column',
+        'align-items': 'flex-end',
+        alignItems: 'flexEnd',
+      },
+
+      title: {
+        fontSize: 16,
+        color: '#444',
+
+        emphasis: {
+          fontSize: 22,
+          fontWeight: 700,
+          color: '#8838FF',
+        }
+      },
+      logo: {
+        width: 250,
+        height: 45,
+      },
+    },
+
+    body: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+
+    content: {
+      display: 'flex',
+      justifyContent: 'center',
+      color: '#444',
+      fontSize: 22,
+      marginBottom: 25,
+
+      emphasis: {
+        textTransform: 'capitalize',
+        fontSize: 30,
+        marginBottom: 25,
+        fontWeight: 700,
+        color: '#8838FF',
+      }
+    },
+
+    footer: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      alignContent: 'center',
+      width: 100+'%',
+      marginTop: 10+'vh',
+      paddingTop: 20,
+      paddingBottom: 20,
+      paddingLeft: 20,
+      paddingRight: 20,
+      backgroundColor: '#efefef',
+
+      column: {
+        textTransform: 'capitalize',
+        display: 'flex',
+        flexDirection: 'column',
+        alignContent: 'center',
+        alignItems: 'center',
+
+        content: {
+          color: '#444',
+          fontSize: 17,
+          marginBottom: 15,
+
+          emphasis: {
+            fontSize: 19,
+            color: '#8838FF',
+            fontWeight: 600,
+          }
+        }
+      }
+    },
+  }
+});
+
+export default styles;
