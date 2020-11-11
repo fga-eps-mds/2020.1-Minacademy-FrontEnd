0ce83058e3b383c1edc2944099b0a9372b1bfe02
estevaoreis <estevaoreis1587@gmail.com>
2020-11-11 13:10:46 -0300

    Criação da página de Confirmação de Cadastro

---
+++ src/views/RegisterConfirm/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.scss';
+import { useHistory, useParams } from 'react-router-dom';
+import { changeUserEmail, logout} from '../../services/usersService';
+
+/* eslint-disable no-shadow */
+function RegisterConfirm(email) {
+  return (
+    <div>
+      <h1>Confirmação de cadastro</h1>
+      <p>Um email foi enviado para o e-mail {email} para validar seu cadastro.</p>
+      <p>Acesse sua caixa de e-mails e siga as instruções contidas no e-mail.</p>
+    </div>
+    
+  )
+};
+
+export default RegisterConfirm;
