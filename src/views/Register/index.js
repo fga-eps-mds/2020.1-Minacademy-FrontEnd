54473a788f85e39672f9edd00a5624b30a22d1b0
estevaoreis <estevaoreis1587@gmail.com>
2020-11-16 11:18:30 -0300

    Valor 'Feminino' predefinido no cadastro

--- src/views/Register/index.js
+++ src/views/Register/index.js
@@ -20,6 +20,7 @@ function Register({ registerRequest, isLoading }) {
   const { handleSubmit, register, watch, setValue, errors, setError } = useForm({
     defaultValues: {
       userType: null,
+      gender: 'Female',
     },
   });
   const history = useHistory();
