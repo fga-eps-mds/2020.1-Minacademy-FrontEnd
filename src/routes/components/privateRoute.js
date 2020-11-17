8883e01f69b72dec8ebb7e94690048d38dc611b5
Eduardo Picolo <eduardonpicolo@gmail.com>
2020-11-17 06:11:23 -0300

    Routes: Adicionado animações de transição para rotas
    Co-authored-by: luucas-melo <lucasmelodos322@gmail.com>

--- src/routes/components/privateRoute.js
+++ src/routes/components/privateRoute.js
@@ -1,37 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Route, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { selectCurrentUser } from '../../slices/usersSlice';
+import RouteTransition from '../../UI/animation/RouteTransition'
 
-const PrivateRoute = ({ currentUser, children, exact, path }) => (
-  <>
-    <Route exact={exact} path={path}>
-      {currentUser ? (
-        <div className="animation">{children}</div>
-      ) : (
-        <Redirect to="/login" />
-      )}
-    </Route>
-  </>
-);
+const PrivateRoute = ({ currentUser, children, exact=false, path, dispatch, ...rest }) => {
+  return (
+  <RouteTransition exact={exact} path={path} {...rest}>
+    {currentUser
+    ? children
+    : <Redirect push to="/login" />}
+  </RouteTransition>
+)};
 
 PrivateRoute.defaultProps = {
   currentUser: null,
-  exact: false
+  exact: false,
 };
 
 PrivateRoute.propTypes = {
   currentUser: PropTypes.oneOfType([PropTypes.oneOf([null]), PropTypes.object]),
   children: PropTypes.elementType.isRequired,
   exact: PropTypes.bool,
-  path: PropTypes.string.isRequired
+  path: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   currentUser: selectCurrentUser(state),
 });
 
-export default connect(mapStateToProps, null, null, { pure: true })(
-  PrivateRoute
+export default (
+  connect(mapStateToProps)(PrivateRoute)
 );
--- src/routes/components/publicRoute.js
+++ src/routes/components/publicRoute.js
@@ -1,42 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Route, Redirect, useLocation } from 'react-router-dom';
+import { Redirect} from 'react-router-dom';
 import { selectCurrentUser } from '../../slices/usersSlice';
+import RouteTransition from '../../UI/animation/RouteTransition'
 
-const PublicRoute = ({ currentUser, children, exact, path }) => {
-  const location = useLocation();
-  return (
-    <>
-      <Route exact={exact} path={path}>
-        {currentUser ? ( // eslint-disable-line no-nested-ternary
-          location.pathname === '/login' ? (
-            <Redirect to="/dashboard" />
-          ) : (
-            <Redirect to="/bem-vindo" />
-          )
-        ) : (
-          <div className="animation">{children}</div>
-        )}
-      </Route>
-    </>
-  );
+const PublicRoute = ({ currentUser, children, exact, path, dispatch, ...otherProps }) => {
+   return (
+     <RouteTransition exact={exact} path={path} {...otherProps}>
+      {currentUser
+      ? otherProps.location.pathname === '/login'
+      ? <Redirect push to="/dashboard" />
+      : <Redirect push to="/bem-vindo" />
+      : children
+      }
+     </RouteTransition>
+   );
 };
 
 PublicRoute.defaultProps = {
   currentUser: null,
-  exact: false
+  exact: false,
 };
 
 PublicRoute.propTypes = {
   currentUser: PropTypes.oneOfType([PropTypes.oneOf([null]), PropTypes.object]),
   children: PropTypes.elementType.isRequired,
   exact: PropTypes.bool,
-  path: PropTypes.string.isRequired
+  path: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   currentUser: selectCurrentUser(state),
 });
 
-export default connect(mapStateToProps)(PublicRoute);
+export default (connect(mapStateToProps)(PublicRoute));
