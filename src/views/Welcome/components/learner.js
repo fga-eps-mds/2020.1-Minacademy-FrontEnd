84ab14baa1fd3ec351272fd5d9f8e9a4b412416b
Rodrigo <ro12062000@gmail.com>
2020-11-18 15:41:23 -0300

    Editado o texto de bem-vindo para nova aprendiz

--- src/views/Welcome/components/learner.js
+++ src/views/Welcome/components/learner.js
@@ -8,13 +8,20 @@ function Female(){
         <div id='welcome-learner' className='welcome-learner'>
             <h1>Seja Bem-Vinda</h1>
             <br/>
-            <p>Agora que seu cadastro foi validado, você tem acesso a várias coisas dentro da plataforma</p>
-            <p>Você pode por exemplo, acessar o nosso tutorial, e em cada módulo, responder nossas atividade</p>
-            <p>Você pode solicitar um de nossos mentores para te auxiliar no que for preciso, tendo um chat disponível para que possam se comunicar, para que você possa concluir todo o tutorial</p>
-            <p>Ao final de todo o tutorial você receberá um certificado e ainda terá a chance de se tornar mais uma de nossas incríveis monitoras
-              e ajudar muitas outras pessoas
+            <p>Você já foi cadastrada com sucesso na plataforma Minacademy, e agora poderá explorar tudo que temos para oferecer!</p>
+            <p>Vamos começar pelo principal, para você que esta cadastrada como aprendiz, você poderá acessar o nosso tutorial,
+              e em cada módulo, responder nossas atividades.</p>
+            <p>Caso queira, a nossa plataforma também tem uma ótima opção para quem está começando neste mundo da programação,
+              você poderá solicitar um de nossos mentores para te auxiliar no que for preciso, contando com um chat disponível
+              para que vocês possam se comunicar, com o intuito de te ajudar a concluir todo o tutorial, da melhor maneira possível.
+              E se em algum momento você se sentir desconfortável com seu/sua mentor(a), você poderá solicitar para que remova-a(o)
+              como mentor(a) a qualquer momento, para evitar aborrecimentos, vocês não serão mais vinculados novamente.</p>
+            <p>E ao concluir todas as atividades do tutorial você receberá um certificado da Minacademy, que poderá ser consultado,
+              compartilhado e baixado quando você quiser. Não somente isso como você será automáticamente promovida para mentora
+              na plataforma, e caso queira poderá se tornar mais uma de nossas incríveis mentoras e ajudar muitas outras aprendizes.
             </p>
-            <p>Agora vai lá explora essa plataforma e mergulhe nesse maravilhoso mundo da programação</p>
+            <p>Agora que entendeu como a nossa plataforma funciona, você pode prosseguir para a nossa dashboard, onde terá acesso à
+              principais funcionalidades para você, vamos lá começar a explorar o nosso site?</p>
         </div>
         <div className='links'>
             <br/>
