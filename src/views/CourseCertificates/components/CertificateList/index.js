038946ad077606ecb78d78010072a3c35e8d094a
luucas-melo <lucasmelodos322@gmail.com>
2020-11-18 12:55:44 -0300

    Bugfix: arrumado bug que loader nao aparecia
    -titulo do certificado modificado de acordo com pdf
    
    Co-authored-by: EduardoPicolo <eduardonpicolo@gmail.com>

--- src/views/CourseCertificates/components/CertificateList/index.js
+++ src/views/CourseCertificates/components/CertificateList/index.js
@@ -12,7 +12,7 @@ function CertificateList({ certificate }) {
     <div className="certificateList">
       <div className="certificateList__body">
         {certificate.courseType === 'Learner' && (
-          <h3>Certificado de Conclusão do Tutorial Minacademy</h3>
+          <h3>Certificado de Conclusão do Tutorial</h3>
         )}
         {certificate.courseType === 'Mentor' && (
           <h3>Certificado de Mentoria</h3>
