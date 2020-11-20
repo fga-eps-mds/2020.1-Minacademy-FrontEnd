7d87df85e08b33ca8a3da890b021e08b4a5bfbef
estevaoreis <estevaoreis1587@gmail.com>
2020-11-20 15:28:37 -0300

    Modificações no RegisterConfirm

--- src/views/RegisterConfirm/index.js
+++ src/views/RegisterConfirm/index.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { unwrapResult } from '@reduxjs/toolkit';
+import './style.scss';
 import { useHistory, useParams } from 'react-router-dom';
 import { registerUser } from '../../services/usersService';
 import { isLoading } from '../../slices/usersSlice';
+import { openWebSocket } from '../../services/websocket';
 import Loader from '../../components/Loader';
-import './style.scss';
 
 /* eslint-disable no-shadow */
 function RegisterConfirm({ registerUser, isLoading }) {
