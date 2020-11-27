0d2431d9dc34fba3b62c7219ded8b13f2e8657e8
Rodrigo <ro12062000@gmail.com>
2020-11-27 01:53:17 -0300

    Adicionado conclusão de tutorial, ao responder corretamente
    Co-authored-by: EduardoPicolo <eduardonpicolo@gmail.com>
    Co-authored-by: luucas-melo <lucasmelodos322@gmail.com>

--- cypress/integration/e2e/learner/tutorial.spec.js
+++ cypress/integration/e2e/learner/tutorial.spec.js
@@ -58,18 +58,41 @@ const TutorialIntegration = () => describe('Tutorial', () => {
     }}
 
     cy.get('.question__buttons').get('button[type="submit"]').as('submitButton')
+
+    cy.wait(500)
     cy.get('#question').within(() => {
       cy.get('input').check('b')
     })
+
+    cy.wait(500)
     cy.get('@submitButton').should('not.be.disabled')
 
     cy.wait(500)
 
+    cy.intercept('POST', '/api/certificates', {
+      body: {
+        id: "5fc07ddb1c18fa00154e0130",
+        user: "5fb835b05acb1c001550620f",
+        courseType: "Learner",
+        key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmI4MzViMDVhY2IxYzAwMTU1MDYyMGYiLCJpYXQiOjE2MDY0NTA2NTF9.Q1GTDeWMNTfoQS1naHGlOlAShuj5Z0st55B1sKh-PcE",
+        createdAt: "2020-11-27T04:17:31.941Z",
+        updatedAt: "2020-11-27T04:17:31.941Z",
+      }
+    })
     cy.intercept('POST', '/api/answer', staticResponse)
-    cy.intercept('GET', '/api/progress', { statusCode: 400, body: { error: 'ERROR PROGRESS!' }})
+    cy.intercept('GET', '/api/progress', { 
+      statusCode: 200,
+      body: {
+        questionsResults: [staticResponse.body], 
+        totalProgress: 100
+      },
+      // delayMs: 1000
+    })
     cy.get('@submitButton').click()
     cy.wait(500)
     cy.get('.question__result').contains('h2', 'CORRETO!')
+
+    cy.get('button').contains('cancelar').wait(1000).click()
   })
 })
 
